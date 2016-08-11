c12421ef10e2776b6dba5f37b6142d2f1ba04ce6
Omar Segura <omarseguvi@gmail.com>
2016-08-10 20:25:22 -0600

    Cambio de nombre de carpeta

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ let express = require('express'); //importar modulo de express
 let path = require('path'); //importar modulo de path
 let app = express();
 
-app.use('/scripts', express.static(__dirname + '/web/js')); //Para que pueda leer las rutas de los scripts
-app.use('/cssFiles', express.static(__dirname + '/web/css')); //Para que pueda leer las rutas de los stilos
+app.use('/scripts', express.static(__dirname + '/Web/js')); //Para que pueda leer las rutas de los scripts
+app.use('/cssFiles', express.static(__dirname + '/Web/css')); //Para que pueda leer las rutas de los stilos
 //Se puede filtrar los get con expresiones regulares
-app.get('/',(request,response) => response.sendFile('index.html',{root:path.join(__dirname,'./web')}))
+app.get('/',(request,response) => response.sendFile('index.html',{root:path.join(__dirname,'./Web')}))
 app.listen(1333,() => console.log("Server is running on port 1333"));
