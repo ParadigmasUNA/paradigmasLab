cdc35f00bc00627502a10715f41dced8bb4d113b
Omar Segura <omarseguvi@gmail.com>
2016-08-11 23:47:48 -0600

    Actualizando server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ let app = express();
 app.use('/scripts', express.static(__dirname + '/Web/js')); //Para que pueda leer las rutas de los scripts
 app.use('/cssFiles', express.static(__dirname + '/Web/css')); //Para que pueda leer las rutas de los stilos
 //Se puede filtrar los get con expresiones regulares
-app.get('/',(request,response) => response.sendFile('index.html',{root:path.join(__dirname,'./Web')}))
+app.get('/',(request,response) => response.sendFile('index.html',{root:path.join(__dirname,'./Web')}));
 app.listen(1333,() => console.log("Server is running on port 1333"));
