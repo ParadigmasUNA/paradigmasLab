e1e48ae77817ebd0dd00920a036acb64a6a70af2
chino2152 <fabian.hernandez.ch@gmail.com>
2016-08-16 11:46:14 -0600

    probando github .i.

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ app.use('/cssFiles', express.static(__dirname + '/Web/css')); //Para que pueda l
 //Se puede filtrar los get con expresiones regulares
 app.get('/',(request,response) => response.sendFile('index.html',{root:path.join(__dirname,'./Web')}));
 app.listen(1333,() => console.log("Server is running on port 1333"));
+
+
+
