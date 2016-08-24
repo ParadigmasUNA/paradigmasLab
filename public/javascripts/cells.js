a9a2a139894c2d4b7ea6a3cb12573e17d92c7d8a
Omar Segura <omarseguvi@gmail.com>
2016-08-24 15:07:37 -0600

    Primera Solucion maze, falta recargar

--- public/javascripts/cells.js
+++ public/javascripts/cells.js
@@ -4,6 +4,7 @@ class Cell{
     this.j = j;
     this.paredes = [true,true,true,true]; //0->Arriba | 1->Derecha | 2->Abajo | 3-> Izquierda
     this.visited = false;
+    this.path = 0;
     this.checkNeighbors = (grid, neighbors, dif) => {
       let top = grid[index(this.i,this.j-1,dif)];
       let rigth = grid[index(this.i+1,this.j,dif)];
@@ -22,7 +23,40 @@ class Cell{
       //Retorna un vecino aleatorio, sino tiene devuelve undefined
       return (neighbors.length > 0) ? neighbors[Math.floor(Math.random() * neighbors.length)] : undefined;
     }
+
+    this.checkWalls = (grid, neighbors, dif) => {
+      let top = grid[index(this.i,this.j-1,dif)];
+      let rigth = grid[index(this.i+1,this.j,dif)];
+      let bottom = grid[index(this.i,this.j+1,dif)];
+      let left = grid[index(this.i-1,this.j,dif)];
+
+      if(!this.paredes[0] && top && !top.visited)
+          neighbors.push(top) //Ingresa el de arriba
+      if(!this.paredes[1] && rigth && !rigth.visited)
+          neighbors.push(rigth) //Ingresa el derecha
+      if(!this.paredes[2] && bottom && !bottom.visited)
+          neighbors.push(bottom) //Ingresa el de abajo
+      if(!this.paredes[3] && left && !left.visited)
+          neighbors.push(left) //Ingresa el de left
+
+      return (neighbors.length > 0) ? neighbors[Math.floor(Math.random() * neighbors.length)] : undefined;
+    }
   }
 }
+
 //Me dice si el indice esta fuera de los valores de la matriz
 let index = (x,y,tamano) => (x < 0 || y < 0 || x > tamano-1 || y > tamano-1) ? -1 : x + y * tamano;
+
+let toCell = e =>{
+  let cell = new Cell(e.i,e.j);
+  cell.paredes = e.paredes;
+  return cell;
+}
+
+let toCellSol = e =>{
+  let cell = new Cell(e.i,e.j);
+  cell.paredes = e.paredes;
+  cell.path = e.path;
+  cell.visited = e.visited;
+  return cell;
+}
